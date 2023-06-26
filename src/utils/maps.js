60172e3ee682efae18174dcc791d59d1805028df
Juan Manuel Gallegos <110039627+Waanma@users.noreply.github.com>
2023-06-26 14:43:57 -0300

    Hidding api key

--- src/utils/maps.js
+++ src/utils/maps.js
@@ -1,4 +1,4 @@
-export const MAPS_API_KEY = "AIzaSyBczPFi32DSF6E97m4-lAgSAEzBT-XHQGM";
+export const MAPS_API_KEY = "";
 
 export const URL_MAPS = (lat, lng, zoom = 14) =>
   `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=600x300&maptype=roadmap
